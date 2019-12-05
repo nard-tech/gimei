d344d1178f6f8b02a1189805ad61d97669124026
willnet <netwillnet@gmail.com>
2019-12-05 17:59:00 +0900

    Don't use Gimei::Unique::UniqueGenerator.clear

--- lib/gimei/unique_generator.rb
+++ lib/gimei/unique_generator.rb
@@ -2,30 +2,21 @@ class Gimei
   class RetryLimitExceeded < StandardError; end
 
   class UniqueGenerator
-    @marked_unique = Set.new
-
-    class << self
-      attr_reader :marked_unique
-
-      def clear
-        marked_unique.each(&:clear)
-        marked_unique.clear
-      end
-    end
-
     def initialize(max_retries)
       @max_retries = max_retries
       @previous_results = Hash.new { |hash, key| hash[key] = Set.new }
     end
 
-    def clear
-      previous_results.clear
+    def clear(key = nil)
+      if key
+        previous_results(key.to_sym).clear
+      else
+        previous_results.clear
+      end
     end
 
     %i[name last first address prefecture city town].each do |method_name|
       define_method method_name do
-        self.class.marked_unique.add(self)
-
         max_retries.times do
           result = Gimei.public_send(method_name)
 
